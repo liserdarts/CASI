728f199fd35da2a604629cdb43a86cf1c06784ad
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-02-06 03:18:40 +0000

    Added console output to the sample project

--- Sample/MainWindow.xaml.vb
+++ Sample/MainWindow.xaml.vb
@@ -26,7 +26,68 @@ Class MainWindow
     End Sub
 
     Private Sub UXRun_Click(sender As Object, e As RoutedEventArgs) Handles UXRun.Click
+        For Each Child In FindChildren(Of PropertyEditorLibrary.PropertyEditor)(UxPropertyObjects)
+            Child.IsEnabled = False
+        Next
+        UXRun.IsEnabled = False
+        UxResults.Visibility = Windows.Visibility.Visible
+
+        Dim Thread As New System.Threading.Thread(AddressOf Run)
+        Thread.IsBackground = True
+        Thread.Name = "ScriptRunner"
+        Thread.Start
+    End Sub
+
+    Private Sub Run()
+        Console.SetOut(New TextBoxWriter(UxLog))
         Runner.Run
+        Console.WriteLine("Done")
+
+        Dispatcher.Invoke(New Action(AddressOf Finish))
+    End Sub
+    Private Sub Finish()
+        UxProgress.Visibility = Windows.Visibility.Collapsed
+    End Sub
+
+    Private Function FindChildren(Of T As DependencyObject)(Parent As FrameworkElement) As List(Of T)
+        Dim Children As New List(Of T)
+        
+        For I As Integer = 0 To VisualTreeHelper.GetChildrenCount(Parent) - 1
+            Dim Cur As DependencyObject
+            Cur = VisualTreeHelper.GetChild(Parent, I)
+            
+            If TypeOf Cur Is T Then
+                Children.Add(Cur)
+            Else
+                Children.AddRange(FindChildren(Of T)(Cur))
+            End If
+        Next
+        
+        Return Children
+    End Function
+
+End Class
+
+Public Class TextBoxWriter
+    Inherits IO.TextWriter
+    
+    Dim Txt As Primitives.TextBoxBase
+    Public Sub New(Txt As Primitives.TextBoxBase)
+        Me.Txt = Txt
+    End Sub
+
+    Public Overrides ReadOnly Property Encoding() As Text.Encoding
+        Get
+            Return Text.Encoding.Default
+        End Get
+    End Property
+
+    Public Overrides Sub Write(Value As Char)
+        If Not Txt.Dispatcher.CheckAccess Then
+            Txt.Dispatcher.BeginInvoke(New Action(Of Char)(AddressOf Write), Value)
+            Return
+        End If
+        Txt.AppendText(Value)
     End Sub
 
 End Class
