769b5b8d9f63e49229a904b02f886cd83eb85af7
liserdarts_cp <SND\liserdarts_cp>
2011-02-06 03:26:35 +0000

    Moved the TextBoxWriter class to its own file

--- Sample/MainWindow.xaml.vb
+++ Sample/MainWindow.xaml.vb
@@ -67,27 +67,3 @@ Class MainWindow
     End Function
 
 End Class
-
-Public Class TextBoxWriter
-    Inherits IO.TextWriter
-    
-    Dim Txt As Primitives.TextBoxBase
-    Public Sub New(Txt As Primitives.TextBoxBase)
-        Me.Txt = Txt
-    End Sub
-
-    Public Overrides ReadOnly Property Encoding() As Text.Encoding
-        Get
-            Return Text.Encoding.Default
-        End Get
-    End Property
-
-    Public Overrides Sub Write(Value As Char)
-        If Not Txt.Dispatcher.CheckAccess Then
-            Txt.Dispatcher.BeginInvoke(New Action(Of Char)(AddressOf Write), Value)
-            Return
-        End If
-        Txt.AppendText(Value)
-    End Sub
-
-End Class
---
+++ Sample/TextBoxWriter.vb
@@ -0,0 +1,23 @@
+﻿Public Class TextBoxWriter
+    Inherits IO.TextWriter
+    
+    Dim Txt As Primitives.TextBoxBase
+    Public Sub New(Txt As Primitives.TextBoxBase)
+        Me.Txt = Txt
+    End Sub
+
+    Public Overrides ReadOnly Property Encoding() As Text.Encoding
+        Get
+            Return Text.Encoding.Default
+        End Get
+    End Property
+
+    Public Overrides Sub Write(Value As Char)
+        If Not Txt.Dispatcher.CheckAccess Then 'Run on the UI thread
+            Txt.Dispatcher.BeginInvoke(New Action(Of Char)(AddressOf Write), Value)
+            Return
+        End If
+        Txt.AppendText(Value)
+    End Sub
+
+End Class
