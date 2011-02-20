6d14b5bec59ecae7f38bd844dc35d76434680dfc
liserdarts_cp <SND\liserdarts_cp>
2011-02-20 10:06:11 +0000

    Corrected the FilePattern in the sample project

--- Sample/MainWindow.xaml.vb
+++ Sample/MainWindow.xaml.vb
@@ -14,7 +14,7 @@ Class MainWindow
     
     Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         Finder.Assembly = Me.GetType.Assembly
-        Finder.FilePattern = ".*sql"
+        Finder.FilePattern = ".*sql$"
         
         Runner = New ScriptRunner
         Runner.Finder = Finder
