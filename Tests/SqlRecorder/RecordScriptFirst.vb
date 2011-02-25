ee6951fc18c015cb9795e98482d2473898f69d70
liserdarts_cp <SND\liserdarts_cp>
2011-02-25 20:03:53 +0000

    Wrote a test to make sure the SqlRecorder can run RecordScript before HasRunScript

---
+++ Tests/SqlRecorder/RecordScriptFirst.vb
@@ -0,0 +1,21 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://casi.codeplex.com/license
+
+Namespace SqlRecorder
+''' <summary>
+''' Make sure RecordScript can be called before HasRunScript
+''' </summary>
+Public Class RecordScriptFirst
+    Inherits Base
+    
+    Protected Overrides Sub CreateRecorder()
+        MyBase.CreateRecorder
+
+        Recorder.RecordScript("RecordScriptFirst")
+    End Sub
+
+End Class
+End Namespace
