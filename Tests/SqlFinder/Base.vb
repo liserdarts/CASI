161035bf0f0989b1929d195b99e254b1f3a931fc
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-02-26 02:48:43 +0000

    Moved everything into new Trunk folder

---
+++ Tests/SqlFinder/Base.vb
@@ -0,0 +1,29 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://scripthelper.codeplex.com/license
+
+Namespace SqlFinder
+Public Class Base
+    Inherits TestFramework.TestCase
+    
+    Protected Finder As New Sql.SqlFinder
+
+    Public Overrides Sub Test()
+        CreateFinder
+
+        Dim Scripts = Finder.GetAllScripts
+        AssertGreater(Scripts.Count, 0)
+
+        For Each Path In Scripts
+            AssertNotNothing(Finder.Open(Path))
+        Next
+    End Sub
+
+    Protected Overridable Sub CreateFinder()
+        Finder = New Sql.SqlFinder
+    End Sub
+
+End Class
+End Namespace
