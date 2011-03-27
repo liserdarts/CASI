695d40518e556850cd9baa901c4ad32ab36c72c4
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-03-27 08:34:27 +0000

    Created CASI.UI project for common ui stuff and moved the property editor from the sample project there

---
+++ Trunk/UI/GenericDesigner.xaml.vb
@@ -0,0 +1,47 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://casi.codeplex.com/license
+
+Public Class GenericDesigner
+
+    Public Property ItemsSource() As IEnumerable(Of ScriptProperty)
+        Get
+            Return UxPropertyObjects.ItemsSource
+        End Get
+        Set
+            UxPropertyObjects.ItemsSource = Value
+        End Set
+    End Property
+
+    Dim LIsReadOnly As Boolean
+    Public Property IsReadOnly() As Boolean
+        Get
+            Return LIsReadOnly
+        End Get
+        Set
+            LIsReadOnly = Value
+            For Each Child In FindChildren(Of PropertyEditorLibrary.PropertyEditor)(UxPropertyObjects)
+                Child.IsEnabled = Not Value
+            Next
+        End Set
+    End Property
+    
+    Private Function FindChildren(Of T As DependencyObject)(Parent As FrameworkElement) As List(Of T)
+        Dim Children As New List(Of T)
+        
+        For I As Integer = 0 To VisualTreeHelper.GetChildrenCount(Parent) - 1
+            Dim Cur As DependencyObject
+            Cur = VisualTreeHelper.GetChild(Parent, I)
+            
+            If TypeOf Cur Is T Then
+                Children.Add(Cur)
+            Else
+                Children.AddRange(FindChildren(Of T)(Cur))
+            End If
+        Next
+        
+        Return Children
+    End Function
+End Class
