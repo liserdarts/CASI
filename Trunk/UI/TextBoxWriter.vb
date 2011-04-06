a08b294fe7123cf5b231ab11c2dd8f7b8b2e725c
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-04-06 18:44:15 +0000

    Made some formatting changes

--- Trunk/UI/TextBoxWriter.vb
+++ Trunk/UI/TextBoxWriter.vb
@@ -1,4 +1,10 @@
-﻿Public Class TextBoxWriter
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://casi.codeplex.com/license
+
+Public Class TextBoxWriter
     Inherits IO.TextWriter
     
     Dim Txt As Primitives.TextBoxBase
