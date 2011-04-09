79fb9fa32a15ac4384591ca27a9d9e57c0f5f832
liserdarts_cp <SND\liserdarts_cp>
2011-04-09 12:34:48 +0000

    Fixed resizing problems in the sample project

---
+++ Trunk/UI/WidthConverter.vb
@@ -0,0 +1,17 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://casi.codeplex.com/license
+
+Public Class WidthConverter
+    Implements IValueConverter
+
+    Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+        Return System.Convert.ToDouble(value) - System.Convert.ToDouble(parameter)
+    End Function
+
+    Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+        Return System.Convert.ToDouble(value) + System.Convert.ToDouble(parameter)
+    End Function
+End Class
