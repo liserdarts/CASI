3788257ea7be22c7737df7a575f0abfc63eb6eb1
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2011-04-15 12:59:37 +0000

    Changed the ConsolePropertyParser class to be able to build a string to set the properties later

--- Trunk/Tests/Console/ParseProperties.vb
+++ Trunk/Tests/Console/ParseProperties.vb
@@ -5,26 +5,38 @@
 'http://casi.codeplex.com/license
 
 Namespace Console
+''' <summary>
+''' Use the ParseProperties class to set properties on ScriptProperty objects
+''' </summary>
 Public Class ParseProperties
     Inherits TestFramework.TestCase
     
+    Protected Executor As TestExecutor
+    Protected Runner As ScriptRunner
+
     Public Overrides Sub Test()
-        Dim Executor As New TestExecutor
+        CreateRunner
+        Parse("-TestScriptProperty.TestProperty=TestValue", "TestValue")
+    End Sub
+
+    Protected Overridable Sub CreateRunner()
+        Executor = New TestExecutor
         
-        Dim Runner As New ScriptRunner
+        Runner = New ScriptRunner
         Runner.Executor = Executor
         Runner.Finder = New TestFinder
         Runner.Recorder = New TestRecoder
         Runner.Transaction = New TestTransactionProvider
-        
+
+        Runner.GetPropertyObjects
+    End Sub
+
+    Protected Sub Parse(Args As String, Value As String)
         Dim Parser As New ConsolePropertyParser(Runner)
         AssertIsFalse(Parser.Parse(New String(){}))
 
-        AssertIsFalse(Executor.TestProperty.Open)
-        Dim Args As New List(Of String)
-        Args.Add("-TestScriptProperty.TestProperty=TestValue")
-        AssertIsTrue(Parser.Parse(Args))
-        AssertIsEqual(Executor.TestProperty.TestProperty, "TestValue")
+        AssertIsTrue(Parser.Parse(Args.Split(" ")))
+        AssertIsEqual(Executor.TestProperty.TestProperty, value)
     End Sub
 End Class
 End Namespace
