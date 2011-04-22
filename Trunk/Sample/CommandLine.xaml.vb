f0d477ec45184381bf40eb9e61fb9bbeace32169
liserdarts_cp <SND\liserdarts_cp>
2011-04-22 16:41:28 +0000

    Created a UI control that builds command line arguments based on what the properties are already set to

---
+++ Trunk/Sample/CommandLine.xaml.vb
@@ -0,0 +1,14 @@
+﻿Public Class CommandLine
+
+    Public Sub New(Runner As ScriptRunner)
+        ' This call is required by the designer.
+        InitializeComponent()
+        
+        UxCommand.DataContext = Runner
+    End Sub
+
+    Private Sub UxClose_Click(sender As Object, e As RoutedEventArgs) Handles UxClose.Click
+        Close
+    End Sub
+
+End Class
