cbb616b5f73c40d87f7d9f0f370b5d11a6ce1ede
liserdarts_cp <SND\liserdarts_cp>
2011-05-01 17:07:24 +0000

    Created debugger UI that allows running 1 script at a time, or recording a script without running it.

---
+++ Trunk/UI/Debugger.xaml.vb
@@ -0,0 +1,30 @@
+﻿'Copyright (c) 2011, Nicholas Avery
+'Licensed under the Microsoft Public License (Ms-PL)
+'you may not use this file except in compliance with the License.
+'You may obtain a copy of the license at 
+'http://casi.codeplex.com/license
+
+Public Class Debugger
+    Dim LBatch As ScriptBatch
+    Public Property Batch() As ScriptBatch
+        Get
+            Return LBatch
+        End Get
+        Set
+            LBatch = Value
+            ShowScripts
+        End Set
+    End Property
+
+    Private Sub ShowScripts()
+        If Batch Is Nothing Then
+            UxScripts.ItemsSource = Nothing
+            Return
+        End If
+
+        Dim Scripts = Batch.Template.Finder.GetAllScripts
+        Scripts = Batch.Template.Sorter.Sort(Scripts)
+        UxScripts.ItemsSource = Scripts.Reverse
+    End Sub
+
+End Class
