f72fe2d5a39d2585ddf44e3416a8698102a6c008
liserdarts_cp <116813+liserdarts@users.noreply.github.com>
2013-03-03 09:25:42 +0000

    Added time remaining to progress bar

--- Trunk/UI/Progress.xaml.vb
+++ Trunk/UI/Progress.xaml.vb
@@ -1,10 +1,14 @@
-﻿'Copyright (c) 2011, Nicholas Avery
+﻿'Copyright (c) 2011-2013, Nicholas Avery
 'Licensed under the Microsoft Public License (Ms-PL)
 'you may not use this file except in compliance with the License.
 'You may obtain a copy of the license at 
 'http://casi.codeplex.com/license
 
 Public Class Progress
+
+    Dim LastProgress As ProgressChangedEventArgs
+    Dim StartTime As Date
+    Dim WithEvents Timer As System.Threading.Timer
 
     Dim WithEvents LBatch As ScriptBatch
     Public Property Batch() As ScriptBatch
@@ -44,6 +48,44 @@ Public Class Progress
         End Select
 
         UxProgress.Value = e.Progress * 100
-        UxStage.Text = e.GetStageText
+        
+        If LastProgress Is Nothing OrElse LastProgress.Stage <> e.Stage Then
+            StartTime = Date.Now
+        End If
+        LastProgress = e
+        
+        If Timer Is Nothing Then
+            Timer = New System.Threading.Timer(AddressOf UpdateTime)
+        End If
+        If UxProgress.IsIndeterminate Then
+            Timer.Change(0, System.Threading.Timeout.Infinite)
+        Else
+            Timer.Change(0, 1000)
+        End If
+    End Sub
+
+    Private Sub UpdateTime()
+        Try
+            Dim TimeSpent = Date.Now - StartTime
+            
+            If TimeSpent.TotalSeconds < 2 Or LastProgress.Progress = 0 Then
+                SetText(LastProgress.GetStageText)
+                Return
+            End If
+
+            Dim TimeLeft = TimeSpan.FromTicks(TimeSpent.Ticks * (1 / LastProgress.Progress) * (1 - LastProgress.Progress))
+            Dim Text = "{0} ({1} Second(s) Remaining)"
+
+            SetText(String.Format(Text, LastProgress.GetStageText, Math.Round(TimeLeft.TotalSeconds)))
+        Catch Ex As Exception
+        End Try
+    End Sub
+
+    Private Sub SetText(Text As String)
+        If Not Dispatcher.CheckAccess Then
+            Dispatcher.Invoke(New Action(Of String)(AddressOf SetText), Text)
+            Return
+        End If
+        UxStage.Text = Text
     End Sub
 End Class
